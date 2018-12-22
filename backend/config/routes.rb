35687a61b700646e27437b3c6294a23b7784cd6a
Arup Rakshit <aruprakshit@rocketmail.com>
2018-12-22 16:32:16 +0530

    Add backend route definition for searches

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :todos
+  resources :todos do
+    collection do
+      post :search
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
