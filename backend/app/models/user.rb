56661fd01f0765f0b90ac604b732b508f41ee316
Arup Rakshit <aruprakshit@rocketmail.com>
2018-12-26 20:14:58 +0530

    Add slug field to user table with first and last name

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  extend FriendlyId
+
   has_secure_password
+  friendly_id :username, use: :slugged
 
   has_one_attached :avatar
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_26_134759) do
+ActiveRecord::Schema.define(version: 2018_12_26_144329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,10 @@ ActiveRecord::Schema.define(version: 2018_12_26_134759) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "slug"
+    t.string "first_name"
+    t.string "last_name"
+    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
 end
